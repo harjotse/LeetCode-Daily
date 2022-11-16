4f2aa1a276c4813631fc8422d9e6c262b1f2b085
Harjot singh <bhatiaharjot2000@gmail.com>
2022-11-17 01:29:16 +0530

    16 Nov challenge done

--- 16Nov_GuessThePick.java
+++ 16Nov_GuessThePick.java
@@ -13,4 +13,34 @@
             return n;
         }
     }
+
+    //
+    /* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+// approach 2 Actual done 
+// binary search 
+// q is very simple 
+// 1 to n is range ok
+// now just send mid in guess and it will give -1 1 acc
+// do binary search acc to that simple
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if (res == 0)
+                return mid;
+            else if (res < 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+}
     //https://leetcode.com/problems/guess-number-higher-or-lower/
