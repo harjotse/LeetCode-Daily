96e400f55a7ff5112536a414639da5f6f65f2ea6
Harjot singh <84695958+harjotse@users.noreply.github.com>
2023-01-06 17:48:41 +0000

    06 jan done

---
+++ 06jan.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int count=0;
+        int leftOver=coins;
+        Arrays.sort(costs);
+        for(int i=0;i<costs.length;i++)
+        {
+            if(leftOver-costs[i]>=0)
+            {
+                count++;
+                leftOver-=costs[i];
+            }
+        }
+        return count;
+    }
+}
+//https://leetcode.com/problems/maximum-ice-cream-bars/
